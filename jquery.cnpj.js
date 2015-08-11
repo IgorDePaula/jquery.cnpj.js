e7c3d10a42d6de9ca3efc6d11b300ceb6a71979e
Igor C. de Paula <principe.borodin@gmail.com>
2015-08-11 15:45:19 -0300

    Finish

--- jquery.cnpj.js
+++ jquery.cnpj.js
@@ -1,23 +1,70 @@
-(function($){
-    $.fn.cnpj = function(callback){
-         
-        $(this).on('keyup',function(){
-           var val = $(this).val().replace(/\D+/g,'');           
-           if(pureVal.length === 12){
-               
-           }           
+/**
+ * Plugin para validação de cnpj em query
+ * Data: 11/08/2015
+ * Base do código: http://www.geradorcnpj.com/javascript-validar-cnpj.htm. Acesso: 11/08/2015
+ */
+(function ($) {
+    $.fn.cnpj = function (callback) {
+
+        $(this).on('blur', function () {
+            var val = $(this).val().replace(/[^\d]+/g, '');
+            if (val.length === 14) {
+                if (!isValid(val)) {
+                    return callback();
+                }
+            }
         });
-        
-        var firstDigit = function(val){
-            
-        };
-        
-        var secondDigit = function(val){
-            
-        };
-        
-        var isValid = function(val){
-            
+
+        var isValid = function (cnpj) {
+            if (cnpj === '')
+                return false;
+
+            if (cnpj.length !== 14)
+                return false;
+
+            // Elimina CNPJs invalidos conhecidos
+            if (cnpj === "00000000000000" ||
+                    cnpj === "11111111111111" ||
+                    cnpj === "22222222222222" ||
+                    cnpj === "33333333333333" ||
+                    cnpj === "44444444444444" ||
+                    cnpj === "55555555555555" ||
+                    cnpj === "66666666666666" ||
+                    cnpj === "77777777777777" ||
+                    cnpj === "88888888888888" ||
+                    cnpj === "99999999999999")
+                return false;
+
+            // Valida DVs
+            tamanho = cnpj.length - 2
+            numeros = cnpj.substring(0, tamanho);
+            digitos = cnpj.substring(tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2)
+                    pos = 9;
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado !== digitos.charAt(0))
+                return false;
+
+            tamanho = tamanho + 1;
+            numeros = cnpj.substring(0, tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2)
+                    pos = 9;
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado !== digitos.charAt(1))
+                return false;
+
+            return true;
+
         };
     };
 })(jQuery);
