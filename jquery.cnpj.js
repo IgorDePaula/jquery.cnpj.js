a1793fd1c9d670274f63df43bd65800a7f9ab7b2
Igor C. de Paula <principe.borodin@gmail.com>
2015-08-11 15:18:22 -0300

    Add skeleton of plugin

--- jquery.cnpj.js
+++ jquery.cnpj.js
@@ -1,3 +1,23 @@
-Jquery.fn.cnpj = function(callback){
-    
-}
+(function($){
+    $.fn.cnpj = function(callback){
+         
+        $(this).on('keyup',function(){
+           var val = $(this).val().replace(/\D+/g,'');           
+           if(pureVal.length === 12){
+               
+           }           
+        });
+        
+        var firstDigit = function(val){
+            
+        };
+        
+        var secondDigit = function(val){
+            
+        };
+        
+        var isValid = function(val){
+            
+        };
+    };
+})(jQuery);
